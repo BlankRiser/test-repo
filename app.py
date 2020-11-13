6098b3fa2c857b698be4b9e9f50f2ada4ceed18f
Ram Shankar Choudhary <rschoudhary1999@gmail.com>
2020-11-13 11:07:57 +0530

    hopes up

--- app.py
+++ app.py
@@ -1,72 +1,55 @@
 import os
-from flask_sqlalchemy import SQLAlchemy
 
+os.environ["DJANGO_SETTINGS_MODULE"] = "proj.settings"
 
+import django  # noqa: E402 module level import not at top of file
+
+django.setup()
 
 from flask import Flask, request, json
+from hello.models import GithubData
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgres://mdbtssfxfrfmbt:85e19ba21e968ac1f3b04a6da1fa2cbe629fed7ab4851f143e85b3728fd16b89@ec2-34-200-106-49.compute-1.amazonaws.com:5432/d9o5stbu9dopan'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
-from models import *
 
-@app.route('/', methods=['GET', 'POST', 'PUT','HEAD','DELETE'])
-def retrieveData():
+# any method
+# any URL
+# @app.route('/')
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT','HEAD','DELETE']) 
+def api_gh_msg(path):
+    """
+    Flask lets you get all the methods only when mentioned explicitly 
+    or by the default would be GET method
+    WebHook connector that get gets the details which GitHub posts.
+    (Still working on it)
+    Returns:
+        dictionary: JSON that is sent to the above route by GitHub to subscribed events.
+    """
+    print(request.method)
+    headers = request.headers
     # parameters = request.args #returns ImmutableMultiDict([])
     # print("parameters: ", parameters)
-
-    method =request.method
-    print(request.method)
-
-    headers = request.headers
-    headers = str(headers)
-    print("\nheaders: ", headers)
-
-    url = request.url
-    print("Path: ", url)
-
-    # body = request.json 
-    # json can be easier to parse later but make sure request.headers['Content-type'] is 'application/json'
     body = request.json
     body = str(body)
+    method =request.method
+    url = request.url
+    print("Url: ", url)
+    print("\nheaders: ", headers)
+    
+    # body = request.json 
+    # json can be easier to parse later but make sure request.headers['Content-type'] is 'application/json'
+    body = request.data 
+    GithubData.objects.create(path=url, method=method, headers=headers, body=body)
 
-    webhook_data = Webhook_data(path =url,method = method,headers = headers,body = body)
-    db.session.add(webhook_data)
-    db.session.commit()
-    
     return "ok"
-    
+
+
+
     # if request.headers['Content-type'] == 'application/json':
     #    with open('data.json', 'w', encoding='utf-8') as f:
     #        json.dump(request.json, f, ensure_ascii=False, indent=4)
     #    return json.dumps(request.json) 
-
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT','HEAD','DELETE']) 
-def retrieveDataParam(path):
-
-    method =request.method
-    print(request.method)
-
-    headers = request.headers
-    headers = str(headers)
-    print("\nheaders: ", headers)
-
-    url = request.url
-    print("Path: ", url)
-
-    # body = request.json 
-    # json can be easier to parse later but make sure request.headers['Content-type'] is 'application/json'
-    body = request.json
-    body = str(body)
-
-    webhook_data = Webhook_data(path =url,method = method,headers = headers,body = body)
-    db.session.add(webhook_data)
-    db.session.commit()
-    
-    # GithubData.objects.create(path=url, method=method, headers=headers, body=body)
-    return "ok"
 
 if __name__ =='__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from app import db
 
-class Webhook_data(db.Model):
+
+
+
+class webhook_data(db.Model):
     __tablename__ = 'github_webhook_data'
     
     id = db.Column(db.Integer, primary_key=True)
@@ -9,4 +12,20 @@ class Webhook_data(db.Model):
     headers = db.Column(db.String())
     body = db.Column(db.String())
 
-    # db.create_all()
+    def __init__(self, path, method, headers, body):
+        self.path = path
+        self.method = method
+        self.headers = headers
+        self.body = body
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'path': self.path,
+            'method': self.method,
+            'headers,':self.headers,
+            'body,':self.body,
+        }
